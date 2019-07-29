b1697c5a19037ce0647a0cdd251364c4cb17217f
iphearum <phearumgic@gmail.com>
2019-07-29 17:40:37 +0700

    fix something to work well

--- core/views.py
+++ core/views.py
@@ -66,7 +66,7 @@ def test_api(request):
     request_from = '104.8912,11.5684'    # lng, lat
     request_end = '104.8834,11.504'  # lng, lat
     route = ["routed-bike","routed-car"]
-    response = requests.get('https://routing.openstreetmap.de/'+route[0]+'/route/v1/driving/'+request_from+';'+request_end+'?overview=false&geometries=polyline&steps=true')
+    response = requests.get('https://routing.openstreetmap.de/'+route[0]+'/route/v1/driving/'+request_end+';'+request_from+'?overview=false&geometries=polyline&steps=true')
     # response = requests.get("https://routing.openstreetmap.de/routed-car/route/v1/driving/104.8912,11.5684;104.8834,11.504?overview=false&geometries=polyline&steps=true")
     # response = requests.get("https://api.openrouteservice.org/v2/directions/cycling-road?api_key=5b3ce3597851110001cf624878418d2388b344e4a0036d25d0581bc1&start=104.883438,11.504154&end=104.891045,11.568393")
     
